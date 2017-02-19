938d44100023bbd7b9be63ef9b5ecd23d22b354e
Jérôme <chrom12@hotmail.com>
2017-02-19 19:05:42 +0100

    Add svm forecast with rolling windows, direct and recursive strategy

--- server.R
+++ server.R
@@ -1,5 +1,7 @@
 library(shiny)
+library(plotly)
 library(e1071)
+library(caret)
 library("RColorBrewer")
 source("C:/Users/GG/Desktop/Memoire/myrep/ErrorMeasures.R")
 
@@ -8,6 +10,19 @@ df <- read.csv('histoCAC40_2Y.CSV')
 df$date <- as.Date(df$date)
 df <- df[order(as.Date(df$date, format="%y/%m/%d")),]
 df$target <- df$lastPrice
+
+volume <- read.csv(paste(dataDirectory, 'cac40_dataset_volume.CSV', sep=""), sep = "")
+sigma_4 <- read.csv(paste(dataDirectory, 'cac40_6y_dataset_sigma_4.csv', sep=""), sep = "")
+rv_50 <- read.csv(paste(dataDirectory, 'cac40_dataset_rv_50.csv', sep=""), sep = "")
+ccreturn <- read.csv(paste(dataDirectory, 'cac40_dataset_ccreturn.csv', sep=""), sep = "")
+volume$date <- as.Date(volume$Index)
+sigma_4$date <- as.Date(sigma_4$Index)
+rv_50$date <- as.Date(rv_50$Index)
+ccreturn$date <- as.Date(ccreturn$Index)
+names(volume) <- gsub("X", "", names(volume))
+names(sigma_4) <- gsub("X", "", names(sigma_4))
+names(rv_50) <- gsub("X", "", names(rv_50))
+names(ccreturn) <- gsub("X", "", names(ccreturn))
 
 
 getColors <- function(nb){
@@ -30,66 +45,189 @@ getString <- function(costs, gammas, MASES){
   res
 }
 
-getSVM <- function(df, cost, gamma){
-  model <- svm(target ~ date, gamma = 2^gamma, cost = 2^cost, data = df)
-  predict(model, df)
-}
 
+shinyServer(function(input, output, session) {
 
-shinyServer(function(input, output) {
-  
+  window <- reactive({input$rwindow})
+  trainingset <- reactive({input$trainingset})
+  cost <- reactive({2^input$cost})
+  gamma <- reactive({2^input$gamma})
+  days <- reactive({input$days})
+
+  # Send dataset choices to the ui
+  updateSelectInput(session, "market",
+                    choices = names(volume)[-c(1, length(volume))],
+                    selected = head(names(volume)[-c(1)], 1)
+  )
+  updateSelectInput(session, "dataset",
+                    choices = c("volume", "sigma_4", "rv_50", "ccreturn"),
+                    selected = "volume"
+  )
+
+  # Generate a summary of the data
+  output$summary <- renderPrint({
+    summary(getDf()$target)
+  })
+
+  # Gets data with the chosen id
   getDf <- function(){
-    chosenIid = unique(df$iid)[input$nb]
-    df <- df[df$iid == chosenIid,]
-    df
+    if (input$dataset == "volume"){
+        df <- volume[c("date", input$market)]
+    } else if (input$dataset == "sigma_4"){
+        df <- sigma_4[c("date", input$market)]
+    } else if (input$dataset == "rv_50"){
+        df <- rv_50[c("date", input$market)]
+    } else if (input$dataset == "ccreturn"){
+        df <- ccreturn[c("date", input$market)]
+    }
+    names(df) <- c("date", "target")
+    head(tail(df, input$days[2]), input$days[2] - input$days[1])
   }
-  
+
+  getTestDf <- reactive({
+    tail(getDf(), trainingset())
+  })
+
+  getTrainDf <- reactive({
+    head(getDf(), -trainingset())
+  })
+
+  getSVM <- function(cost, gamma){
+    model <- svm(target ~ date, gamma = 2^gamma, cost = 2^cost, data = getTrainDf())
+    predict(model, getTestDf())
+  }
+
   params <- reactiveValues(costs = NULL, gammas = NULL, MASES = NULL)
-  
+
   observeEvent(input$addModel, {
     params$costs <- c(params$costs, input$cost)
     params$gammas <- c(params$gammas, input$gamma)
-    predictedY <- getSVM(getDf(), input$cost, input$gamma)
-    params$MASES <- c(params$MASES, MASE(getDf()$target, predictedY))
+    predictedY <- getSVM(input$cost, input$gamma)
+    params$MASES <- c(params$MASES, MASE(getTestDf()$target, predictedY))
   })
-  
+
   observeEvent(input$resetModels, {
     params$costs <- NULL
     params$gammas <- NULL
     params$MASES <- NULL
   })
-  
+
   output$modelList <- renderText({ 
     if (length(params$gammas) > 0){
       HTML(getString(params$costs, params$gammas, params$MASES))
     }
   })
-  
-  output$distPlot <- renderPlot({
 
-    df <- getDf()
+  output$dataPlot <- renderPlotly({
     
-    plot(target ~ date, data = df, type = 'l',
-         main = paste("Plot of the market iid : ", unique(df$iid), sep = ""))
-    
-    nbModels <- length(params$costs)
-    if (nbModels > 0){
-      for (i in 1:nbModels){
-        colors <- getColors(nbModels)
-        predictedY <- getSVM(df, params$costs[i], params$gammas[i])
-        points(df$date, predictedY, col = colors[i], type='l', lwd=1)
-      }
+    if (input$market != ""){
+      df <- getDf()
+
+      plot_ly(y = df$target, x = df$date, type = 'scatter', mode = 'lines', name = 'Target') %>%
+
+        add_trace(x = head(getTestDf()$date, 1), y = c(min(df$target), max(df$target)),
+                mode = 'lines', line = list(dash = "dash"), name = 'Training set cut') %>%
+
+        layout(title = paste("Market : ", input$market, sep = ""))
     }
-    
-    if (input$applySVM){
-      model <- svm(target ~ date, gamma = 2^input$gamma, cost = 2^input$cost, data = df)
-      predictedY <- predict(model, df)
-      points(df$date, predictedY, col = "red", type='l', lwd=1)
-    } 
-    if (input$applyLinearModel){
-      model <- lm(target~ date, df)
-      predictedY <- predict(model, df)
-      points(df$date, predictedY, col = "blue", type='l', lwd=1)
+    else{
+      # Returns a default empty plot while data isn't available
+      plot_ly(data.frame(Date = as.Date(character())), type = 'scatter', mode = 'lines')
     }
   })
+
+  output$predPlot <- renderPlotly({
+    
+    if (input$market != ""){
+      df <- getDf()
+
+      # Initial plot with legends
+      p <- plot_ly(y = df$target, x = df$date, type = 'scatter', mode = 'lines', name = 'Target')
+      p <- p %>% layout(title = paste("Out-of-sample forecast of market : ", input$market, sep = ""))
+
+      # Training set vertical cut
+      p <- p %>% add_trace(x = head(getTestDf()$date, 1), y = c(min(df$target), max(df$target)),
+                           mode = 'lines', line = list(dash = "dash"), name = 'Training set cut')
+
+      nbModels <- length(params$costs)
+      if (nbModels > 0){
+        for (i in 1:nbModels){
+          colors <- getColors(nbModels)
+          predictedY <- getSVM(params$costs[i], params$gammas[i])
+          p <- p %>% add_trace(x = getTestDf()$date, y = predictedY, 
+                               mode = 'lines', name = paste('SVM model : ', i, sep = ''))
+        }
+      }
+
+      if (input$applySVM){
+        predictedY <- svmForecast()
+        p <- p %>% add_trace(x = getTestDf()$date, y = predictedY, 
+                             mode = 'lines', name = 'SVM forecast')
+      } 
+
+      if (input$applyNaiveModel){
+        predictedY <- naiveForecast()
+        p <- p %>% add_trace(x = getTestDf()$date, y = predictedY, 
+                             mode = 'lines', name = 'Linear model')
+      }
+    }
+    else{
+      # Returns a default empty plot while data isn't available
+      p <- plot_ly(data.frame(Date=as.Date(character())), type = 'scatter', mode = 'lines')
+    }
+    p
+  })
+
+  output$predTable <- renderTable(rownames = TRUE, bordered = TRUE, {
+    svmPredictedY <- svmForecast()
+    naivePredictedY <- naiveForecast()
+    data.frame(RMSE = c(RMSE(svmPredictedY, getTestDf()$target), RMSE(naivePredictedY, getTestDf()$target)),
+               MAE = c(MAE(svmPredictedY, getTestDf()$target), MAE(naivePredictedY, getTestDf()$target)),
+               MAPE = c(MAPE(svmPredictedY, getTestDf()$target), MAPE(naivePredictedY, getTestDf()$target)),
+               MASE = c(MASE(svmPredictedY, getTestDf()$target), MASE(naivePredictedY, getTestDf()$target)),
+               NMSE = c(NMSE(svmPredictedY, getTestDf()$target), NMSE(naivePredictedY, getTestDf()$target)),
+               row.names = c("SVM Errors", "Naive Errors"))
+  })
+
+  output$predErrorPlot <- renderPlotly({
+    predictedY <- svmForecast()
+    plot_ly(y = getTestDf()$target - predictedY, x = getTestDf()$date, type = 'scatter', mode = 'lines', name = 'Error')%>% 
+      layout(title = "SVM forecast error", showlegend = TRUE)
+  })
+
+  svmForecast <- function(){
+
+    datestoPredict <- getTestDf()["date"]
+
+    if (input$strategy == "direct"){
+      predictedY <- c()
+      for(i in seq_along(1:nrow(datestoPredict))){
+        rolling_window <- head(tail(getTrainDf(), nrow(datestoPredict) + window() - i), window())
+        model <- svm(target ~ date, gamma = gamma(), cost = cost(), data = rolling_window)
+
+        datetoPredict <- data.frame(date = datestoPredict[i,])
+        pred <- predict(model, datetoPredict)
+        predictedY <- c(predictedY, pred)
+      }
+    }
+
+    else if (input$strategy == "recursive"){
+      rolling_window <- tail(getTrainDf(), window())[, c("date", "target")]
+      predictedY <- c()
+      for(i in seq_along(1:nrow(datestoPredict))){
+
+        model <- svm(target ~ date, gamma = gamma(), cost = cost(), data = rolling_window)
+        datetoPredict <- data.frame(date = datestoPredict[i,])
+        pred <- predict(model, datetoPredict)
+        predictedY <- c(predictedY, pred)
+
+        rolling_window <- rbind(rolling_window[-1,], data.frame(date = datetoPredict, target = pred))
+      }
+    }
+    predictedY
+  }
+
+  naiveForecast <- function(){
+    rep(mean(tail(getTrainDf()$target, 30)), nrow(getTestDf()))
+  }
 })
--- ui.R
+++ ui.R
@@ -1,37 +1,81 @@
 library(shiny)
+library(plotly)
 
-shinyUI(pageWithSidebar(
+shinyUI(
   
-  # Application title
-  headerPanel("Stock market"),
+  fluidPage(
   
-  sidebarPanel(
-    sliderInput("nb", 
-                "Which market :", 
-                min = 1, max = 40, value = 1),
-    
-    sliderInput("cost", 
-                "Parameter Cost : (2^X)", 
-                min = -3, max = 7, value = 0),
-    
-    sliderInput("gamma",
-                "Parameter Gamma : (2^X)", 
-                min = -3, max = 7, value = 0),
-
-    checkboxInput("applySVM", "Show SVM", FALSE),
-
-    checkboxInput("applyLinearModel", "Show Linear Model", FALSE),
-
-    actionButton("addModel", "Add Model"),
-    actionButton("resetModels", "Reset"),
-
-    htmlOutput("modelList")
-
-  ),
-  
-  
-  # Shows the plot generated
-  mainPanel(
-    plotOutput("distPlot")
+    # Application title
+    titlePanel("Stock market"),
+        
+    tabsetPanel(type = "tabs", 
+      # Shows the plot generated
+      tabPanel("Data inspection",
+        sidebarLayout(
+          sidebarPanel(
+            selectInput('market', 'Which market :', c()),
+             
+            selectInput('dataset', 'Which dataset :', c()),
+            
+            sliderInput("days",
+                       "Cut data set : ",
+                       min = 1, max = 1000, value = c(1, 365)),
+            
+            sliderInput("trainingset",
+                       "Cut training set : ",
+                       min = 1, max = 1000, value = 30)
+          ),
+          
+          mainPanel(
+            h4(""),
+            plotlyOutput("dataPlot"),
+            h4("Summary"),
+            verbatimTextOutput("summary")
+          )
+        )
+      ),
+      
+      tabPanel("SVM forecasting",
+        sidebarLayout(
+          sidebarPanel(
+            sliderInput("cost", 
+                        "Parameter Cost : (2^X)", 
+                        min = -3, max = 7, value = 0),
+            
+            sliderInput("gamma",
+                        "Parameter Gamma : (2^X)", 
+                        min = -3, max = 7, value = 0),
+            
+            sliderInput("rwindow",
+                        "Rolling window size :", 
+                        min = 30, max = 365, value = 50),
+            
+            radioButtons("strategy","Forecasting strategy", 
+                         c("Recursive" = "recursive", "Direct" = "direct"), inline=T),
+            
+            checkboxInput("applySVM", "Show SVM", TRUE),
+            
+            checkboxInput("applyNaiveModel", "Show Naive Model", FALSE),
+            
+            actionButton("addModel", "Add Model"),
+            actionButton("resetModels", "Reset"),
+            
+            htmlOutput("modelList")
+          ),
+          
+          mainPanel(
+            h4(""),
+            plotlyOutput("predPlot"),
+            tableOutput("predTable"),
+            plotlyOutput("predErrorPlot")
+          )
+        )
+      )
+    )
   )
-))
+)
+
+
+
+
+
